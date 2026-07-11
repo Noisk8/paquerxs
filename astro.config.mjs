b0584abb299cf48ca0a72aa7f06f7417c2449941
noisk8 <juansk8jaramillo@gmail.com>
2026-07-11 05:07:54 -0500

    fix: downgrade to Astro 4 for Node 20 + add output server config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT || '4321')
